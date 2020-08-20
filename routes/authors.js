112603fce7aac0f5cefe76fd22691dcf4ac460b6
Buremoh <51365028+Buremoh@users.noreply.github.com>
2020-08-20 22:24:50 +0000

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -9,9 +9,8 @@ router.get("/", async (req, res) => {
     searchOptions.name = new RegExp(req.query.name, "i");
   }
   try {
-    const authors = await Author.find( searchOptions);
-    res.render("authors/index", { authors: authors, 
-      searchOptions: req.query });
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
   } catch {
     res.redirect("/");
   }
